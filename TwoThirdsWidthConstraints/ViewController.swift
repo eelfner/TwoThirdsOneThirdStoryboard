6f41277a03dfe9c5d6f55195a48f4d52fba183ff
Eric Elfner <eje@geoderanch.com>
2015-04-08 13:11:23 -0400

    Initial Commit

---
+++ TwoThirdsWidthConstraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwoThirdsWidthConstraints
+//
+//  Created by Eric Elfner on 2015-04-08.
+//  Copyright (c) 2015 Eric Elfner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
